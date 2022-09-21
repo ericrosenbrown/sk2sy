1aac46da227533e23dffc435460292a85c8de17e
eric <eric_rosen@brown.edu>
2022-09-21 10:49:30 -0400

    removed old method for computing factors for domain

--- sk2sy/algorithms/compute_factors.py
+++ sk2sy/algorithms/compute_factors.py
@@ -68,64 +68,8 @@ def compute_factors_from_subgoal_option_transitions(subgoal_options):
 
 	return(factors_state_idxs,factors_options)
 
-def compute_factors_domain(domain, num_transitions:float = 100):
-	'''
-	Compute the factors (parition over state variables based on similar option masks) for a domain. This implicitly assumes
-	the domain already has subgoal options, so this function will sample num_transitions transitions from it to generate
-	masks and then factors
 
-	Parameters:
-	@domain: a domain to compute the factors for
-	@num_transitions (optional): number of transitions to sample from domain to calculate masks for factors.
-
-	Returns:
-	@factors_state_idxs: a dictionary, where key is factor_id, and value is state_idxs where state_idxs
-	is a list of state_idxs associated with factor_id 
-	@factors_options: a dictionary, where key is factor_id, and value is list of options that have mask over
-	state_variables included in factors_state_idxs[factor_id]
-	'''
-	factors_state_idxs = {}
-	factors_options = defaultdict(lambda : [])
-
-	option_mask_dict = {} #dictionary containing keys for options and values being list of masked state variables
-
-	#generate random transitions for the masks
-	transitions = generate_transitions(domain, num_transitions=num_transitions)
-
-	#for each action, calculate the mask
-	for action in domain.actions:
-		print("Calculating mask for action {action}".format(action=action))
-
-		#Get all transitions with the current action we're getting mask for
-		action_transitions: list[Transition] = list(filter(lambda transition: transition.action == action, transitions))
-		#Get the masks for all transitions in action_transitions
-		masks = list(map(lambda transition:get_mask(transition.start_state,transition.end_state), action_transitions))
-		print(masks)
-
-		#use reduce to turn list of masks (list of lists) into just a list of state variables, convert to set and back to list
-		#to get unique state variables
-		option_mask = list(set(functools.reduce(tuple.__add__, masks, tuple())))
-		#sort option mask
-		option_mask.sort() 
-		
-		option_mask_dict[action] = tuple(option_mask)
-
-	print("option mask dict {o}".format(o=option_mask_dict))
-
-	#Compute factors by partioning state variables based on option_mask_dict
-	for option, mask in option_mask_dict.items():
-		if mask not in factors_state_idxs.values(): #a new factor is being made
-			factor_id = len(list(factors_state_idxs.keys()))
-			factors_state_idxs[factor_id] = mask
-		else: #factor already exists that overlaps with this mask, get the factor id associated with mask
-			factor_id = list(factors_state_idxs.values()).index(mask)
-		#add option to dictionary based on factor_id
-		factors_options[factor_id].append(option)
-
-	return(factors_state_idxs,factors_options)
-
-
-def compute_factors_domain_2(domain: Domain, num_transitions:float = 100) -> tuple[dict, dict]:
+def compute_factors_domain(domain: Domain, num_transitions:float = 100) -> tuple[dict, dict]:
 	'''
 	Compute the factors (parition over state variables based on similar option masks) for a domain. This implicitly assumes
 	the domain already has subgoal options, so this function will sample num_transitions transitions from it to generate
@@ -161,7 +105,7 @@ def compute_factors_domain_2(domain: Domain, num_transitions:float = 100) -> tup
 	# ie how many there are.
 	# We could do that in the above loop, but that's gross, so
 	# I'll just leave this until the domain has that property
-	options2statevars = partition_by_function(statevar2options.keys(), lambda s: frozenset(statevar2options[s]))
+	options2statevars: dict[frozenset[str], list[int]] = partition_by_function(statevar2options.keys(), lambda s: frozenset(statevar2options[s]))
 	factor2statevars = {i:vs for i, vs in enumerate(options2statevars.values())}
 	factor2options = {i:x for i, x in enumerate(options2statevars.keys())}
 
